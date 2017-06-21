ceb9f27837179178b80847ed96aa0b06d7f48469
周鑫 <zinx2016@163.com>
2017-06-21 15:32:20 +0800

    Update eventLoop.h

--- src/net/eventLoop.h
+++ src/net/eventLoop.h
@@ -2,24 +2,38 @@
 #define _EXPLORER_EVENT_LOOP_H_
 
 #include "../base/base.h"
-#include "callback.h"
 #include "../base/mutex.h"
+#include "../base/thread.h"
+#include "../base/currentThread.h"
+#include <vector>
+#include <memory>
 
 namespace Explorer {
 
 class Channel;
 class Epoller;
 
-class EventLoop
+class EventLoop : public NoCopy
 {
 public:
+        typedef std::vector<Channel*> ChannelList;
 
+        EventLoop();
+        ~EventLoop();
+
+        // Epoller::epoll_wait()-->Channel::handleEvent();
+        void loop();
+        void quit();
+
+        // Channel::update()-->EventLoop::updateChannel()-->Epoller::updateChannel()
+        void updateChannel(Channel* channel);
 
 private:
+        const pid_t threadId_;
         bool looping_;
         bool quit_;
-        Mutex mutex_;
-
+        std::unique_ptr<Epoller> epoller_;
+        ChannelList activeChannels_;
 };
 
 } // namespace Explorer
