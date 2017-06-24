dade3bfc05ad909b416e2ec3898ca4957e8cb59a
周鑫 <zinx2016@163.com>
2017-06-24 17:24:31 +0800

    Create tcp02.cc

---
+++ test/tcp02.cc
@@ -0,0 +1,38 @@
+/*
+** 演示Explorer网络库部分的定时器功能
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "../net/timerQueue.h"
+#include "../base/currentThread.h"
+#include "../net/eventLoop.h"
+
+using Explorer::EventLoop;
+
+int cnt = 0;
+EventLoop* pl;
+
+void timeout()
+{
+        ++cnt;
+        printf("time out , cnt : %d\n", cnt);
+        if (cnt == 20)
+                pl->quit();
+}
+
+int main()
+{
+        printf("main: pid = %d , tid = %d\n", getpid(), Explorer::CurrentThread::tid());
+
+        EventLoop loop;
+        pl = &loop;
+
+        for(int i = 1; i < 10; i++)
+                loop.runAfter(i, timeout);
+
+        loop.runEvery(2.5, timeout);
+
+        loop.loop();
+}
