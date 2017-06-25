d0af04b8320d83d2ed0a45b249fe9d119ff8d275
周鑫 <zinx2016@163.com>
2017-06-25 17:52:58 +0800

    Update callback.h

--- src/net/callback.h
+++ src/net/callback.h
@@ -3,15 +3,16 @@
 
 #include <functional>
 #include <memory>
+#include "netAddress.h"
 
 namespace Explorer {
 
 class TcpConnection;
 
-typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 typedef std::function<void ()> ReadCallBack;
 typedef std::function<void ()> WriteCallBack;
-typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallBack;
+typedef std::function<void ()> TimerCallBack;
+typedef std::function<void (int sockfd, const NetAddress&)> ConnectionCallBack;
 
 } // namespace Explorer
 
